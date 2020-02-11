324e4bc243d62e69edd8cac37cf90fef85bee657
Aron Gilli <arongilli95@gmail.com>
2020-02-11 18:08:39 +0100

    colonne sculpture

---
+++ NTAA-Reflow/Assets/ColumnMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColumnMovement : MonoBehaviour
+{
+    public Transform giantColumn;
+    Vector3 columnDistance;
+
+    private void Start()
+    {
+         columnDistance = giantColumn.position - this.gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        giantColumn.transform.position = Vector3.Lerp(giantColumn.transform.position, this.gameObject.transform.position + columnDistance, .5f * Time.deltaTime);
+        giantColumn.rotation = Quaternion.Lerp(giantColumn.rotation, this.gameObject.transform.rotation, 5 * Time.deltaTime);
+        if (gameObject.transform.rotation.z > 90 || gameObject.transform.rotation.z < -90 || gameObject.transform.rotation.x > 90 || gameObject.transform.rotation.x < -90)
+        {
+            giantColumn.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
